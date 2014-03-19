30f4cee496abea780d8287e9db9043570fd47c04
AK <ak@laminar.ru>
2014-03-19 13:29:49 +0600

    Добавление. Рабочий вариант без указания кодировок

---
+++ FullExceptionMessage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace replace
+{
+	/// <summary>
+	/// Расширение для получения подного сообщения об ошибке
+	/// </summary>
+	public static class FullExceptionMessage
+	{
+		/// <summary>
+		/// Собирает строку из сообщений переданного и вложенных исключений
+		/// </summary>
+		/// <param name="e">Исключение из которого надо получить сообщение об ошибке</param>
+		/// <returns>Собранные в строку сообщения из исключений</returns>
+		public static string GetMessages(this Exception e)
+		{
+			string msgs = e.GetType() + ". " + e.Message;
+			Exception ex = e;
+			while (ex.InnerException != null)
+			{
+				msgs += ": " + ex.InnerException.Message;
+				ex = ex.InnerException;
+			}
+
+			return msgs;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+namespace replace
+{
+	class Program
+	{
+		static int _replaces = 0;
+
+		static void Main(string[] args)
+		{
+			if (!CmdLineOk(args))
+				return;
+
+			string filename = args[0];
+			string strtoreplace = args[1];
+			string newstr = args[2];
+			string tempfilename = Path.GetTempFileName();
+
+			Console.WriteLine("Replace in {0}: '{1}' -> '{2}'", filename, strtoreplace, newstr);
+
+			try
+			{
+				using(FileStream srcfs = new FileStream(filename,  FileMode.Open, FileAccess.Read))
+				using (FileStream tempfs = new FileStream(tempfilename, FileMode.Open, FileAccess.Write))
+				using(StreamReader srcsr = new StreamReader(srcfs))
+				using (StreamWriter tmpwr = new StreamWriter(tempfs))
+				{
+					string srccurrline;
+					while ((srccurrline = srcsr.ReadLine()) != null)
+					{
+						string deststr = ReplaceSubStr(srccurrline, strtoreplace, newstr);
+						tmpwr.WriteLine(deststr);
+					}
+				}
+
+				Console.WriteLine();
+
+				if (_replaces > 0)
+				{
+					// делаем копию оригинала
+					File.Copy(sourceFileName: filename, destFileName: filename + ".bak", overwrite: true);
+					// удаляем оригинал
+					File.Delete(filename);
+					// переносим временный файл
+					File.Move(sourceFileName: tempfilename, destFileName: filename);
+
+					Console.WriteLine("{0} replaces OK!", _replaces);
+				}
+				else
+				{
+					// удаляем временный файл
+					File.Delete(tempfilename);
+
+					Console.WriteLine("OK. No replaces.");
+				}
+			}
+			catch(Exception ex)
+			{
+				Console.WriteLine("Replace failed:");
+				Console.WriteLine(ex.GetMessages());
+			}
+
+			Console.ReadLine();
+		}
+
+		static string ReplaceSubStr(string srcstr, string strtoreplace, string newstr)
+		{
+			string deststr = srcstr.Replace(strtoreplace, newstr);
+
+			if (deststr != srcstr)
+			{
+				_replaces++;
+				Console.Write("!");
+			}
+			else
+				Console.Write(".");
+
+			return deststr;
+		}
+
+		static bool CmdLineOk(string[] args)
+		{
+			if (args.Count() == 3)
+			{
+				return true;
+			}
+			else
+			{
+				Console.WriteLine(@"Use: replace.exe ""File name.ext"" ""string to replace"" ""new string""");
+
+				return false;
+			}
+		}
+	}
+}
